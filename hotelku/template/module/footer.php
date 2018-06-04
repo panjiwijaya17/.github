eb3b62204ad8f436cdd899ac02334ff6aa48c290
panjiwijaya17 <muhammad1400018145@webmail.uad.ac.id>
2018-06-04 16:49:21 +0700

    versi 1.0

---
+++ hotelku/template/module/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+        </div>
+        <strong>Copyright &copy; 2018 <a href="#">SW Hotel</a>.</strong> All rights reserved.
+      </footer>
